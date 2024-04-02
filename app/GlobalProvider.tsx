d85330184687174608efe39ad5bce6d675e71fcb
Khalil-Zaarour <k.zaarour1997@gmail.com>
2024-04-02 23:41:16 +0300

    5-commit

--- app/GlobalProvider.tsx
+++ app/GlobalProvider.tsx
@@ -1,20 +1,4 @@
 "use client";
-
-// import { store } from "@/redux/store";
-// import { SessionProvider } from "next-auth/react";
-// import { Toaster } from "react-hot-toast";
-// import { Provider } from "react-redux";
-
-// export function GlobalProvider({ children }: { children: React.ReactNode }) {
-//   return (
-//     <>
-//       <Toaster />
-//       <Provider store={store}>
-//         <SessionProvider>{children}</SessionProvider>
-//       </Provider>
-//     </>
-//   );
-// }
 import { store } from "@/redux/store";
 import { Toaster } from "react-hot-toast";
 import { Provider } from "react-redux";

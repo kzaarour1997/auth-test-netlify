508e9311a445a61abbee1643615e53eb193cd9c1
Khalil-Zaarour <k.zaarour1997@gmail.com>
2024-04-03 00:13:50 +0300

    11-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 
-import "./globals.css";
+// import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Head from "./head";

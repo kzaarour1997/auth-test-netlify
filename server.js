d38ca6d4cddb1a8bbbd75e155a92b1645f567269
Khalil-Zaarour <k.zaarour1997@gmail.com>
2024-04-02 16:38:31 +0300

    second commit

--- server.js
+++ server.js
@@ -9,13 +9,6 @@ const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
-// app.use(function (req, res, next) {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
 mongoose
   .connect(DB, {
     useNewUrlParser: true,

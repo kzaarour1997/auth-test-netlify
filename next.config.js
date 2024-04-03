4639b3bf10f93680da8a523bdd93cb8ceecf44fa
Khalil-Zaarour <k.zaarour1997@gmail.com>
2024-04-03 11:31:44 +0300

    first-commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,24 @@
-const withCSS = require("@zeit/next-css");
+// const withCSS = require("@zeit/next-css");
 
-module.exports = withCSS({
-  /* You can add custom configuration options here if needed */
-});
+// module.exports = withCSS({
+//   webpack(config, options) {
+//     // Exclude CSS files from asset module handling
+//     config.module.rules.push({
+//       test: /\.(png|svg|jpg|jpeg|gif)$/i,
+//       type: "asset",
+//       parser: {
+//         dataUrlCondition: {
+//           maxSize: 30 * 1024, // 30kb
+//         },
+//       },
+//     });
+
+//     // Handle CSS files with style-loader and css-loader
+//     config.module.rules.push({
+//       test: /\.css$/,
+//       use: ["style-loader", "css-loader"],
+//     });
+
+//     return config;
+//   },
+// });

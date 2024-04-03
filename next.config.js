eef888e459f01b22635bbe1244682e986dd91def
Khalil-Zaarour <k.zaarour1997@gmail.com>
2024-04-03 06:08:17 +0300

    26-commit

--- next.config.js
+++ next.config.js
@@ -2,15 +2,21 @@ module.exports = (phase, { defaultConfig }) => {
   return {
     webpack: (config, options) => {
       // Modify webpack config here
-      config.module.rules.push({
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset",
-        parser: {
-          dataUrlCondition: {
-            maxSize: 30 * 1024, // 30kb
+      config.module.rules.push(
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset",
+          parser: {
+            dataUrlCondition: {
+              maxSize: 30 * 1024, // 30kb
+            },
           },
         },
-      });
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        }
+      );
 
       return config;
     },

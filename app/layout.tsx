8309e4bde4603e597b4959ef98145f4c17b1dd38
Khalil-Zaarour <k.zaarour1997@gmail.com>
2024-04-03 06:56:19 +0300

    26-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Head from "./head";
--- next.config.js
+++ next.config.js
@@ -1,24 +1,5 @@
-module.exports = (phase, { defaultConfig }) => {
-  return {
-    webpack: (config, options) => {
-      // Modify webpack config here
-      config.module.rules.push(
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset",
-          parser: {
-            dataUrlCondition: {
-              maxSize: 30 * 1024, // 30kb
-            },
-          },
-        }
-        // {
-        //   test: /\.(sa|sc|c)ss$/,
-        //   use: ["style-loader", "css-loader"],
-        // }
-      );
+const withCSS = require("@zeit/next-css");
 
-      return config;
-    },
-  };
-};
+module.exports = withCSS({
+  /* You can add custom configuration options here if needed */
+});
